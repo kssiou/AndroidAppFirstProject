11b773ca2c35aacd1f4d378cc04ef7ded50c6130
kssiou <mouadkssiou@gmail.com>
2023-04-07 11:58:11 +0000

    api volley

--- app/src/main/java/com/a4iir9/MainActivity.java
+++ app/src/main/java/com/a4iir9/MainActivity.java
@@ -2,22 +2,35 @@ package com.a4iir9;
 
 import static com.a4iir9.R.*;
 import static com.a4iir9.R.id.txt1;
+import static com.a4iir9.R.id.txtNom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.DownloadManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.ResponseDelivery;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAGNAME =MainActivity.class.getSimpleName();
+    private static final String HTTP_URL = "https://belatar.name/rest/profile.php?login=test&passwd=test&id=9998";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.activity_main);
-        TextView texte = findViewById(txt1);
+
+        TextView texte = findViewById(txtNom);
 
         texte.setText("here we go !!");
         Log.d(TAGNAME, "on est dans create");
@@ -48,7 +61,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         Log.d(TAGNAME, "on est dans on Resume");
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, HTTP_URL, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d(MainActivity.class.getSimpleName(), response.toString());
+
+            }
+
+        }, new Response.ErrorListener() {
+
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(MainActivity.class.getSimpleName(), error.getMessage());
+
+            }
+        });
+        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(request);
     }
+
 
     @Override
     protected void onRestart() {
---
+++ app/src/main/java/com/a4iir9/Student.java
@@ -0,0 +1,19 @@
+package com.a4iir9;
+
+import android.graphics.Bitmap;
+
+public class Student {
+    private int id ;
+    private String image ;
+    private String nom ;
+    private String email ;
+    private String phone ;
+    private Bitmap photo ;
+
+
+    public Student(){
+
+    }
+
+
+}
