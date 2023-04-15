5d62d454b3b47dade94eae916a847ed2ab384176
kssiou <mouadkssiou@gmail.com>
2023-04-15 00:57:11 +0000

    light change

--- app/src/main/java/com/a4iir9/MainActivity.java
+++ app/src/main/java/com/a4iir9/MainActivity.java
@@ -10,29 +10,29 @@ import android.annotation.SuppressLint;
 import android.app.DownloadManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.ResponseDelivery;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.JsonObjectRequest;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAGNAME =MainActivity.class.getSimpleName();
     private static final String HTTP_URL = "https://belatar.name/rest/profile.php?login=test&passwd=test&id=9998";
+    private Student etd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.activity_main);
-
-        TextView texte = findViewById(txtNom);
-
-        texte.setText("here we go !!");
         Log.d(TAGNAME, "on est dans create");
 
     }
@@ -67,7 +67,39 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(JSONObject response) {
                 Log.d(MainActivity.class.getSimpleName(), response.toString());
 
+                try {
+                    etd = new Student(response.getInt("id"),
+                    response.getString("nom"),response.getString("prenom"),
+                    response.getString("classe"),response.getString("phone"),
+                    null);
+                    VolleySingleton.getInstance(getApplicationContext()).getImageLoader().get(etd.getPhoto(), new ImageLoader.ImageListener() {
+                        @Override
+                        public void onResponse(ImageLoader.ImageContainer response, boolean isImmediate) {
+                            Log.d(TAGNAME, "on est dans on response");
+                        }
+
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.d(TAGNAME, "on est dans on error response");
+                        }
+                    }
+
+                    EditText txtNom = findViewById(id.LabelNom);
+                    EditText txtPrenom = findViewById(id.LabelPrenom);
+                    EditText txtClasse = findViewById(id.Labelclasse);
+                    txtNom.setText(etd.getNom());
+                    txtPrenom.setText(etd.getPrenom());
+                    txtClasse.setText(etd.getClasse());
+
+
+
+                } catch (JSONException e) {
+                   e.printStackTrace();
+                }
+
+
             }
+
 
         }, new Response.ErrorListener() {
 
--- app/src/main/java/com/a4iir9/Student.java
+++ app/src/main/java/com/a4iir9/Student.java
@@ -3,17 +3,72 @@ package com.a4iir9;
 import android.graphics.Bitmap;
 
 public class Student {
-    private int id ;
-    private String image ;
-    private String nom ;
-    private String email ;
-    private String phone ;
-    private Bitmap photo ;
+    private int id;
+
+    private String nom;
+    private String prenom;
+
+    private String classe;
+    private String phone;
+    private Bitmap photo;
 
 
-    public Student(){
+    public Student(int id, String nom, String prenom, String classe, String phone, Bitmap photo) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.classe = classe;
+        this.phone = phone;
+        this.photo = photo;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
 
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getClasse() {
+        return classe;
+    }
+
+    public void setClasse(String classe) {
+        this.classe = classe;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Bitmap getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(Bitmap photo) {
+        this.photo = photo;
+    }
 }
